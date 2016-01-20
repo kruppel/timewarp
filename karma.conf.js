c309404e8d9382f0775db789575222042f7c65ad
Kurt Ruppel <kruppel@zendesk.com>
2016-01-20 08:24:08 -0800

    Initial commit

---
+++ karma.conf.js
@@ -0,0 +1,53 @@
+var customLaunchers = [
+  38,
+  39,
+  40,
+  41,
+  42,
+  43,
+  44,
+  45,
+  46,
+  47,
+  48
+].reduce(function(launchers, version) {
+  launchers['chrome_' + version] = {
+    base: 'SauceLabs',
+    browserName: 'chrome',
+    platform: 'Windows 7',
+    version: version + '',
+    timeZone: 'Asia/Tokyo'
+  };
+
+  return launchers;
+}, {});
+
+module.exports = function(config) {
+  config.set({
+    frameworks: ['mocha'],
+    files: [
+      'node_modules/chai/chai.js',
+      'node_modules/babel-polyfill/dist/polyfill.js',
+      'test/**/*.spec.js'
+    ],
+    preprocessors: {
+      'test/**/*.js': ['babel']
+    },
+    reporters: ['dots', 'saucelabs'],
+    colors: true,
+    autoWatch: true,
+    singleRun: true,
+    concurrency: Infinity,
+    babelPreprocessor: {
+      options: {
+        presets: ['es2015']
+      }
+    },
+    sauceLabs: {
+      testName: 'Date tests',
+      tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER
+    },
+    customLaunchers: customLaunchers,
+    browsers: Object.keys(customLaunchers),
+  })
+};
